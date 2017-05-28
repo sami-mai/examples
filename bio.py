8ee10d32c413f7a14048111557ac9fc6a7780035
sami-mai <samirah.maison@gmail.com>
2017-05-28 17:19:28 +0000

    class code examples

---
+++ bio.py
@@ -0,0 +1,11 @@
+name = "samirah"
+age = 26
+work = "civil engineer"
+why = "cos of money"
+
+print [name, age, work, why]
+
+
+
+
+
---
+++ conditional-flow.py
@@ -0,0 +1,54 @@
+#example 1
+
+# name ="Bertha"
+# female = "Bertha"
+
+# if name == female:
+# 	print "welcome"
+
+# else:
+# 	print "NO"
+
+# #example 2
+
+# name ="Bertha"
+# female = "Alex"
+
+# if name == female:
+# 	print "welcome"
+
+# else:
+# 	print "NO"
+
+
+#example 3
+# name = raw_input("what is your favourite car?")
+
+# if name == "Range Rover":
+# 	print "Tata"
+
+# else:
+# 	print name
+	
+#example 4
+# name = raw_input("what is your favourite car?")
+
+# if name == "Range Rover":
+# 	print "was manufactured by Tata"
+
+# else:
+# 	new_name = raw_input("manufactured by?")
+# 	print "was manufactured by Tata " + new_name
+
+#example 5
+name = raw_input("what is your favourite car?")
+
+if name == "Range Rover":
+	print "was manufactured by Tata"
+
+elif name == "toyota":
+	print "was manufactured by Toyota"
+
+else:
+	print "I don't know"
+
